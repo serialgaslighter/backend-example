d29b45fe3d8e73fd1c606829abd02fe6cfe0c2db
Julian P <julian-peukert@web.de>
2024-11-18 10:28:34 +0100

    get

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.SECRET_EXPAMPLE_PORT;
 const KEY = process.env.SECRET_EXPAMPLE_KEY;
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(`Hallo vom Backend-Server, KEY: ${KEY}`)
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
